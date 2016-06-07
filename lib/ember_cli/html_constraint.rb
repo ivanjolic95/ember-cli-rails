ebc8ef3c1b76081bb6ab5989daebb0187371fa58
Ivan Jolic <ivan.jolic1995@gmail.com>
2016-06-07 10:57:00 +0200

    Generate ember app URL for Googlebot request

--- lib/ember_cli/html_constraint.rb
+++ lib/ember_cli/html_constraint.rb
@@ -1,9 +1,18 @@
 module EmberCli
   class HtmlConstraint
     def matches?(request)
-      matches = request.format.to_s =~ /html/ || -1
+      matches = request.format.to_s =~ /html/ || 
+        user_agent(request) =~ /Google/ || -1
 
       matches > -1
+    end
+
+    def user_agent(request)
+      if request.respond_to?(:user_agent)
+        request.user_agent
+      else
+        ""
+      end
     end
   end
 end
--- spec/lib/ember_cli/html_constraint_spec.rb
+++ spec/lib/ember_cli/html_constraint_spec.rb
@@ -20,14 +20,57 @@ describe EmberCli::HtmlConstraint do
       end
     end
 
-    context %"when the format is empty or nil" do
-      it "return false" do
-        constraint = EmberCli::HtmlConstraint.new
-        nil_request = double(format: nil)
-        empty_request = double(format: "")
+    context %{when the user agent is google bot} do
+      context %{when the format is "html"} do
+        it "return true" do
+          constraint = EmberCli::HtmlConstraint.new
+          request = double(
+            format: :html,
+            user_agent: "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"
+          )
 
-        expect(constraint.matches?(nil_request)).to be false
-        expect(constraint.matches?(empty_request)).to be false
+          expect(constraint.matches?(request)).to be true
+        end
+      end
+      context %{when the format isn't "html"} do
+        it "return true" do
+          constraint = EmberCli::HtmlConstraint.new
+          request = double(
+            format: :json,
+            user_agent: "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"
+          )
+
+          expect(constraint.matches?(request)).to be true
+        end
+      end
+    end
+
+    context %"when the format is empty or nil" do
+      context %{when the user agent is google bot} do
+        it "return true" do
+          constraint = EmberCli::HtmlConstraint.new
+          nil_request = double(
+            format: nil,
+            user_agent: "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"
+          )
+          empty_request = double(
+            format: "",
+            user_agent: "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"
+          )
+
+          expect(constraint.matches?(nil_request)).to be true
+          expect(constraint.matches?(empty_request)).to be true
+        end
+      end
+      context %{when the user agent isn't google bot} do
+        it "return false" do
+          constraint = EmberCli::HtmlConstraint.new
+          nil_request = double(format: nil)
+          empty_request = double(format: "")
+
+          expect(constraint.matches?(nil_request)).to be false
+          expect(constraint.matches?(empty_request)).to be false
+        end
       end
     end
   end
